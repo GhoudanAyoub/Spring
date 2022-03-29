44c6102603238f82b052739a76e2974cd4fbc675
ayoub <ayoubghoudanos@gmail.com>
2022-03-29 22:21:15 +0000

    Contact

---
+++ spring-jpa-1/src/main/java/iir4/g5/cours/springjpa1/controller/ContactController.java
@@ -0,0 +1,53 @@
+package iir4.g5.cours.springjpa1.controller;
+
+import iir4.g5.cours.springjpa1.model.Contact;
+import iir4.g5.cours.springjpa1.repository.ContactRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/Contacts")
+public class ContactController {
+
+    @Autowired
+    ContactRepository ContactRepository;
+
+    @GetMapping("/all")
+    public List<Contact> get(){
+        return ContactRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Contact get(@PathVariable int id){
+        return ContactRepository.findById(id).get();
+    }
+    @GetMapping("/telephone")
+    public ResponseEntity<List<Contact>> findByTelephone(@RequestParam  String telephone){
+        return new ResponseEntity<>(ContactRepository.findByTelephone(telephone), HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public Contact ajouter(@RequestBody Contact u){
+       ContactRepository.save(u);
+       return ContactRepository.findByTelephone(u.getTelephone()).get(0);
+    }
+
+    @DeleteMapping("/{id}")
+    public void suppr(@PathVariable int id){
+        ContactRepository.deleteById(id);
+    }
+
+    @PutMapping
+    public void modif(@RequestBody Contact Contact) {
+        Contact u = ContactRepository.findById(Contact.getId()).get();
+        u.setNom(Contact.getNom());
+        u.setTelephone(Contact.getTelephone());
+        u.setCountryCode(Contact.getCountryCode());
+        u.setEmail(Contact.getEmail());
+    }
+
+}
---
+++ spring-jpa-1/src/main/java/iir4/g5/cours/springjpa1/repository/ContactRepository.java
@@ -0,0 +1,17 @@
+package iir4.g5.cours.springjpa1.repository;
+
+import iir4.g5.cours.springjpa1.model.Contact;
+import iir4.g5.cours.springjpa1.model.utilisateur;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ContactRepository extends JpaRepository<Contact,Integer> {
+    @Query("SELECT c FROM Contact c WHERE c.telephone like  %:telephone%")
+    List<Contact> findByTelephone(@Param("telephone") String telephone);
+}
+
